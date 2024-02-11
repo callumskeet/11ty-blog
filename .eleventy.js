c296cf219dff05ff055132e6931c9454cc02202a
Callum Skeet <github@role.callum.zone>
2024-02-11 11:27:39 +1100

    add tailwind

--- .eleventy.js
+++ .eleventy.js
@@ -1,4 +1,23 @@
+const autoprefixer = require('autoprefixer');
+const postcss = require('postcss');
+const tailwindcss = require('tailwindcss');
+
+/** @param {import('@11ty/eleventy').UserConfig} eleventyConfig */
 module.exports = (eleventyConfig) => {
+    // --- css
+    eleventyConfig.addTemplateFormats('css');
+    eleventyConfig.addExtension('css', {
+        outputFileExtension: 'css',
+        compile: (content, path) => {
+            return async () => {
+                const output = await postcss([
+                    tailwindcss(),
+                    autoprefixer(),
+                ]).process(content, { from: path });
+                return output.css;
+            };
+        },
+    });
     return {
         dir: {
             input: 'src',
