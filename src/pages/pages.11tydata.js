36b34f7005b51b80d6200fd07340c4ef85ebe550
Callum Skeet <github@role.callum.zone>
2024-02-12 00:01:08 +1100

    editable page

--- src/pages/pages.11tydata.js
+++ src/pages/pages.11tydata.js
@@ -2,6 +2,7 @@ module.exports = function () {
     return {
         layout: 'layouts/base',
         tags: ['pages'],
+        content_blocks: [],
         eleventyComputed: {
             permalink: function ({ page }) {
                 return (
