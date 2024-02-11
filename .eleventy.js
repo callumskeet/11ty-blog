c8a990dfa5446889b21507768d5dba8e80b27230
Callum Skeet <github@role.callum.zone>
2024-02-11 18:12:37 +1100

    try alternative bookshop setup

--- .eleventy.js
+++ .eleventy.js
@@ -4,19 +4,25 @@ const tailwindcss = require('tailwindcss');
 const bookshop = require('@bookshop/eleventy-bookshop');
 const bundler = require('@11ty/eleventy-plugin-bundle');
 const { EleventyRenderPlugin } = require('@11ty/eleventy');
+const fg = require('fast-glob');
+
+async function processCss(content, options = {}) {
+    const output = await postcss([tailwindcss(), autoprefixer()]).process(
+        content,
+        options,
+    );
+    return output.css;
+}
 
 /** @param {import('@11ty/eleventy').UserConfig} eleventyConfig */
 module.exports = (eleventyConfig) => {
+    // --- css
     eleventyConfig.addPlugin(EleventyRenderPlugin);
     eleventyConfig.addPlugin(bundler, {
         transforms: [
             async function (content) {
                 if (this.type === 'css') {
-                    const output = await postcss([
-                        tailwindcss(),
-                        autoprefixer(),
-                    ]).process(content);
-                    return output.css;
+                    return processCss(content, { from: this.page.inputPath });
                 }
                 return content;
             },
@@ -26,7 +32,7 @@ module.exports = (eleventyConfig) => {
     // --- bookshop
     eleventyConfig.addPlugin(
         bookshop({
-            bookshopLocations: ['src/_includes/components'],
+            bookshopLocations: ['src/_includes'],
         }),
     );
 
