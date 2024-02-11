cb00074b90b777542ed1456a30ee7e0aef96b2e7
Callum Skeet <github@role.callum.zone>
2024-02-11 23:02:57 +1100

    improve postcss pipeline

--- .eleventy.js
+++ .eleventy.js
@@ -4,30 +4,38 @@ const tailwindcss = require('tailwindcss');
 const bookshop = require('@bookshop/eleventy-bookshop');
 const bundler = require('@11ty/eleventy-plugin-bundle');
 const { EleventyRenderPlugin } = require('@11ty/eleventy');
+const postcssImport = require('postcss-import');
+const postcssImportGlob = require('postcss-import-ext-glob');
+const fs = require('node:fs');
+const crypto = require('node:crypto');
 const fg = require('fast-glob');
 
+/** @returns {Promise<import('postcss').Result>} output */
 async function processCss(content, options = {}) {
-    const output = await postcss([tailwindcss(), autoprefixer()]).process(
-        content,
-        options,
-    );
-    return output.css;
+    const output = await postcss([
+        postcssImportGlob(),
+        postcssImport(),
+        tailwindcss(),
+        autoprefixer(),
+    ]).process(content, options);
+    return output;
+}
+
+function getHash(input) {
+    const secret = 'SALT';
+    const md5Hasher = crypto.createHmac('md5', secret);
+    return md5Hasher.update(input).digest('hex');
 }
 
 /** @param {import('@11ty/eleventy').UserConfig} eleventyConfig */
 module.exports = (eleventyConfig) => {
     // --- css
-    eleventyConfig.addPlugin(EleventyRenderPlugin);
-    eleventyConfig.addPlugin(bundler, {
-        transforms: [
-            async function (content) {
-                if (this.type === 'css') {
-                    return processCss(content, { from: this.page.inputPath });
-                }
-                return content;
-            },
-        ],
-    });
+    // eleventyConfig.addFilter('hash', function (filename) {
+    //     const data = fs.readFileSync('_site/' + filename, {
+    //         encoding: 'utf-8',
+    //     });
+    //     return getHash(data);
+    // });
 
     // --- bookshop
     eleventyConfig.addPlugin(
---
+++ src/css/__styles.11ty.js
@@ -0,0 +1,68 @@
+// This file handles the CSS build.
+// It will run Sass and compile all styles defined in the main entry file.
+
+// main entry point name
+const ENTRY_FILE_NAME = 'main.css';
+
+const path = require('node:path');
+const autoprefixer = require('autoprefixer');
+const postcss = require('postcss');
+const tailwindcss = require('tailwindcss');
+const postcssImport = require('postcss-import');
+const postcssImportGlob = require('postcss-import-ext-glob');
+const fs = require('node:fs');
+const crypto = require('node:crypto');
+
+const oldBundles = new Set();
+
+module.exports = class {
+    async data() {
+        const inputPath = path.join(__dirname, `/${ENTRY_FILE_NAME}`);
+        const content = fs.readFileSync(inputPath, { encoding: 'utf-8' });
+        const output = await this.process(content, {
+            from: inputPath,
+            to: null,
+        });
+        const hash = this.getHash(output.css);
+        const permalink = `/css/bundle.${hash}.css`;
+        this.cleanup(permalink);
+        return {
+            permalink,
+            tags: ['css'],
+            inputPath,
+            content,
+            hash,
+            css: output.css,
+        };
+    }
+
+    cleanup(newPath) {
+        if (oldBundles.size) {
+            for (const path of oldBundles) {
+                try {
+                    fs.rmSync(`_site/${path}`);
+                } catch {}
+            }
+        }
+        oldBundles.add(newPath);
+    }
+
+    getHash(input) {
+        const md5Hasher = crypto.createHmac('md5', '');
+        return md5Hasher.update(input).digest('hex').slice(0, 8);
+    }
+
+    async process(content, options) {
+        const output = await postcss([
+            postcssImportGlob(),
+            postcssImport(),
+            tailwindcss(),
+            autoprefixer(),
+        ]).process(content, options);
+        return output;
+    }
+
+    async render({ css }) {
+        return css;
+    }
+};
