62d313d9453ec8c742a7c2795d400d4e1a65c9e8
Callum Skeet <github@role.callum.zone>
2024-02-11 12:26:03 +1100

    add bookshop

--- .eleventy.js
+++ .eleventy.js
@@ -1,23 +1,35 @@
 const autoprefixer = require('autoprefixer');
 const postcss = require('postcss');
 const tailwindcss = require('tailwindcss');
+const bookshop = require('@bookshop/eleventy-bookshop');
+const bundler = require('@11ty/eleventy-plugin-bundle');
+const { EleventyRenderPlugin } = require('@11ty/eleventy');
 
 /** @param {import('@11ty/eleventy').UserConfig} eleventyConfig */
 module.exports = (eleventyConfig) => {
-    // --- css
-    eleventyConfig.addTemplateFormats('css');
-    eleventyConfig.addExtension('css', {
-        outputFileExtension: 'css',
-        compile: (content, path) => {
-            return async () => {
-                const output = await postcss([
-                    tailwindcss(),
-                    autoprefixer(),
-                ]).process(content, { from: path });
-                return output.css;
-            };
-        },
+    eleventyConfig.addPlugin(EleventyRenderPlugin);
+    eleventyConfig.addPlugin(bundler, {
+        transforms: [
+            async function (content) {
+                if (this.type === 'css') {
+                    const output = await postcss([
+                        tailwindcss(),
+                        autoprefixer(),
+                    ]).process(content);
+                    return output.css;
+                }
+                return content;
+            },
+        ],
     });
+
+    // --- bookshop
+    eleventyConfig.addPlugin(
+        bookshop({
+            bookshopLocations: ['src/_includes/components'],
+        }),
+    );
+
     return {
         dir: {
             input: 'src',
